221d31099d237d91e7f7e17b87ebe1f9c5177c76
molivas2526-afk <molivas2526@politecnics.barcelona>
2026-05-05 10:34:21 +0200

    segundo commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Hola Oriol!");
+        System.out.println("Haciendo merge de la rama b2 a master");
     }
 }
